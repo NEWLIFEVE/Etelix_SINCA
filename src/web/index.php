f243c65e10fc923b148ab9ac28194bb7fa0d9892
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-06 10:06:20 -0430

    haciendo cambios respectivos para ordenar el codigo

--- src/web/index.php
+++ src/web/index.php
@@ -1,7 +1,8 @@
 <?php
 ob_start();
+date_default_timezone_set('America/Caracas');
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/Yii/framework/yii.php';
+$yii='../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
