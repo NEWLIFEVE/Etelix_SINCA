98a7e2f7e7ac7b3cfd52bbda124e533a6328a364
Manuel Zambrano <mmzmm3z@gmail.com>
2014-02-14 14:26:42 -0430

    cambio de conexion

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -82,7 +82,7 @@ return array(
 			'path'=>'upload/importCsv/', // path to folder for saving csv file and file with import params
             ),
 		'db'=>array(
-			'connectionString'=>'mysql:host=localhost;port=3306;dbname=sinca',
+			'connectionString'=>'mysql:host=67.215.160.89;port=3306;dbname=sinca',
 			'emulatePrepare'=>true,
 			'username'=>'root',
 			'password'=>'Nsusfd8263',
