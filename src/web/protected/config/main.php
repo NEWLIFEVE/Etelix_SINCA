872eba5275d46fab0d9d316a2d3c70ba923751aa
Manuel Zambrano <mmzmm3z@gmail.com>
2014-02-20 08:53:25 -0430

    arreglado problema de url manager

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -74,7 +74,7 @@ return array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>/<compania:\w+>'=>'<controller>/<action>/view',
+				'recargas/<action:\w+>/<compania:\w+>'=>'recargas/<action>/view',
 				),
 			),
 		// uncomment the following to use a MySQL database
