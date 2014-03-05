673598fe3c9537df6d7542c07d8828d806686934
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-05 11:07:58 -0430

    prueba

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -84,7 +84,7 @@ return array(
             ),
 		'db'=>array(
 			'class'=>'CDbConnection',
-			'connectionString'=>'mysql:host=67.215.160.89;port=3306;dbname=sinca',
+			'connectionString'=>'mysql:host=localhost;port=3306;dbname=sinca',
 			'emulatePrepare'=>true,
                         'username'=>'root',
 			'password'=>'Nsusfd8263',
