f7f197117dbf675d136a4ba249e61d530b713953
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-06 09:08:26 -0430

    ordenando codigo

--- src/web/protected/components/reportes/captura.php
+++ src/web/protected/components/reportes/captura.php
@@ -1,94 +1,88 @@
 <?php
-
-    class captura extends Reportes 
+/**
+ * @package
+ */
+class captura extends Reportes 
+{
+    public static function reporte($ids) 
     {
-        public static function reporte($ids) 
-        {
-//            $acumuladoSaldoApMov = 0;
-//            $acumuladoSaldoApClaro = 0;
-//            $acumuladoTrafico = 0;
-//            $acumuladoRecargasMov = 0;
-//            $acumuladoRecargasClaro = 0;
-//            $acumuladoDepositos = 0;
-            
-            $balance = captura::get_Model($ids);
-            if($balance != NULL){
-                
-                $table = '<table class="items">'.
-                        Reportes::defineHeader("captura")
-                        .'<tbody>';
-                foreach ($balance as $key => $registro) {
-
-                    $table.=   '<tr >
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.$registro->Fecha.'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.$registro->cabina.'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->MinutosCaptura).'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->TraficoCapturaDollar).'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->Paridad).'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->CaptSoles).'</td>
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->DifSoles,$registro->DifSoles).'</td>   
-                                    <td '.Reportes::defineStyleTd($key+2).'>'.Reportes::defineMonto($registro->DifDollar,$registro->DifDollar).'</td>       
-                                </tr>
-                                ';
-
-                }
-                
-                 $balanceTotals = captura::get_ModelTotal($ids);
-                 $table.=  Reportes::defineHeader("captura")
-                                .'<tr >
-                                        <td '.Reportes::defineStyleTd(2).' id="totalFecha">'.$balanceTotals->Fecha.'</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="todas">Todas</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="vistaAdmin1">'.Reportes::defineTotals($balanceTotals->MinutosCaptura).'</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="vistaAdmin2">'.Reportes::defineTotals($balanceTotals->TraficoCapturaDollar).'</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="totalTrafico">N/A</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="totalRecargaMov">'.Reportes::defineTotals($balanceTotals->CaptSoles).'</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="totalRecargaMov">'.Reportes::defineTotals($balanceTotals->DifSoles,$balanceTotals->DifSoles).'</td>
-                                        <td '.Reportes::defineStyleTd(2).' id="totalRecargaMov">'.Reportes::defineTotals($balanceTotals->DifDollar,$balanceTotals->DifDollar).'</td>      
-                                      </tr>
-                                    </tbody>
-                           </table>';
-            }else{
-                $table='Hubo un error';
-            }
-            return $table;
-        }
-            
-         
-        public static function get_Model($ids) 
-        {
-            $sql = "SELECT b.id as id, b.fecha as Fecha, c.nombre as cabina, 
-		    b.MinutosCaptura as MinutosCaptura,
-		    b.TraficoCapturaDollar as TraficoCapturaDollar,
-		    TRUNCATE(IFNULL(p.Valor,0),2) AS Paridad,
-		    TRUNCATE((IFNULL(b.TraficoCapturaDollar,0)*p.Valor),2) as CaptSoles,
-		    TRUNCATE(((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor)),2) as DifSoles,
-                    TRUNCATE((((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor))/p.Valor),2) as DifDollar
-                    FROM balance as b
-                    INNER JOIN cabina as c ON c.id = b.CABINA_Id
-		    INNER JOIN paridad as p ON p.id = b.PARIDAD_Id
-                    WHERE b.id IN ($ids) 
-                    order by b.fecha desc, c.nombre asc;";
-            
-              return Balance::model()->findAllBySql($sql); 
-         
-        }
+        /*$acumuladoSaldoApMov = 0;
+        $acumuladoSaldoApClaro = 0;
+        $acumuladoTrafico = 0;
+        $acumuladoRecargasMov = 0;
+        $acumuladoRecargasClaro = 0;
+        $acumuladoDepositos = 0;*/
         
-        public static function get_ModelTotal($ids) 
+        $balance=self::get_Model($ids);
+        if($balance != NULL)
         {
-            $sql = "SELECT b.id as id, b.fecha as Fecha, c.nombre as cabina, 
-		    sum(b.MinutosCaptura) as MinutosCaptura,
-		    sum(b.TraficoCapturaDollar) as TraficoCapturaDollar,
-		    sum(p.Valor) as Valor,
-		   sum(TRUNCATE((IFNULL(b.TraficoCapturaDollar,0)*p.Valor),2)) as CaptSoles,
-		   sum(TRUNCATE(((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor)),2)) as DifSoles,
-                   sum(TRUNCATE((((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor))/p.Valor),2)) as DifDollar
-                    FROM balance as b
-                    INNER JOIN cabina as c ON c.id = b.CABINA_Id
-		    INNER JOIN paridad as p ON p.id = b.PARIDAD_Id
-                    WHERE b.id IN ($ids) ";
-            
-              return Balance::model()->findBySql($sql); 
-         
+            $table='<table class="items">'.self::defineHeader("captura").'<tbody>';
+            foreach($balance as $key => $registro)
+            {
+                $table.='<tr>
+                            <td '.self::defineStyleTd($key+2).'>'.$registro->Fecha.'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.$registro->cabina.'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->MinutosCaptura).'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->TraficoCapturaDollar).'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->Paridad).'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->CaptSoles).'</td>
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->DifSoles,$registro->DifSoles).'</td>   
+                            <td '.self::defineStyleTd($key+2).'>'.self::defineMonto($registro->DifDollar,$registro->DifDollar).'</td>       
+                         </tr>';
+            }
+
+            $balanceTotals=self::get_ModelTotal($ids);
+            $table.=self::defineHeader("captura")
+                    .'<tr>
+                        <td '.self::defineStyleTd(2).' id="totalFecha">'.$balanceTotals->Fecha.'</td>
+                        <td '.self::defineStyleTd(2).' id="todas">Todas</td>
+                        <td '.self::defineStyleTd(2).' id="vistaAdmin1">'.self::defineTotals($balanceTotals->MinutosCaptura).'</td>
+                        <td '.self::defineStyleTd(2).' id="vistaAdmin2">'.self::defineTotals($balanceTotals->TraficoCapturaDollar).'</td>
+                        <td '.self::defineStyleTd(2).' id="totalTrafico">N/A</td>
+                        <td '.self::defineStyleTd(2).' id="totalRecargaMov">'.self::defineTotals($balanceTotals->CaptSoles).'</td>
+                        <td '.self::defineStyleTd(2).' id="totalRecargaMov">'.self::defineTotals($balanceTotals->DifSoles,$balanceTotals->DifSoles).'</td>
+                        <td '.self::defineStyleTd(2).' id="totalRecargaMov">'.self::defineTotals($balanceTotals->DifDollar,$balanceTotals->DifDollar).'</td>      
+                      </tr>
+                     </tbody>
+                    </table>';
         }
+        else
+        {
+            $table='Hubo un error';
+        }
+        return $table;
     }
-    ?>
+
+    /**
+     * @access public
+     * @static
+     */
+    public static function get_Model($ids)
+    {
+        $sql="SELECT b.id AS id, b.fecha AS Fecha, c.nombre AS cabina, b.MinutosCaptura AS MinutosCaptura, b.TraficoCapturaDollar AS TraficoCapturaDollar,
+                     TRUNCATE(IFNULL(p.Valor,0),2) AS Paridad,
+                     TRUNCATE((IFNULL(b.TraficoCapturaDollar,0)*p.Valor),2) AS CaptSoles,
+                     TRUNCATE(((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor)),2) AS DifSoles,
+                     TRUNCATE((((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor))/p.Valor),2) AS DifDollar
+              FROM balance AS b INNER JOIN cabina AS c ON c.id = b.CABINA_Id INNER JOIN paridad AS p ON p.id = b.PARIDAD_Id
+              WHERE b.id IN ($ids)
+              ORDER BY b.fecha DESC, c.nombre ASC;";
+        return Balance::model()->findAllBySql($sql);
+    }
+    
+    /**
+     * @access public
+     * @static
+     */
+    public static function get_ModelTotal($ids) 
+    {
+        $sql="SELECT b.id AS id, b.fecha AS Fecha, c.nombre AS cabina, SUM(b.MinutosCaptura) AS MinutosCaptura, SUM(b.TraficoCapturaDollar) AS TraficoCapturaDollar, SUM(p.Valor) AS Valor,
+                     SUM(TRUNCATE((IFNULL(b.TraficoCapturaDollar,0)*p.Valor),2)) AS CaptSoles,
+                     SUM(TRUNCATE(((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor)),2)) AS DifSoles,
+                     SUM(TRUNCATE((((IFNULL(b.FijoLocal,0)+IFNULL(b.FijoProvincia,0)+IFNULL(b.FijoLima,0)+IFNULL(b.Rural,0)+IFNULL(b.Celular,0)+IFNULL(b.LDI,0))-(IFNULL(b.TraficoCapturaDollar,0)*p.Valor))/p.Valor),2)) AS DifDollar
+              FROM balance AS b INNER JOIN cabina AS c ON c.id = b.CABINA_Id INNER JOIN paridad AS p ON p.id = b.PARIDAD_Id
+              WHERE b.id IN ($ids) ";
+        return Balance::model()->findBySql($sql); 
+    }
+}
+?>
