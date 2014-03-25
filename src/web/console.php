6141fcce89b871ce0cdc0dae3a7c4a11bdbcfebd
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-25 12:12:01 -0430

    estableciendo entornos de desarrollo

--- src/web/console.php
+++ src/web/console.php
@@ -1,44 +1,34 @@
 <?php
 date_default_timezone_set('America/Caracas'); 
 //Definimos nuestro servidor de produccion 
-define('SERVER_NAME_PROD','sinca.sacet.com.ve'); 
-//Definimos nuestro servidor de preproduccion 
-define('SERVER_NAME_PRE_PROD','devsinca.sacet.com.ve'); 
-//Definimos nuestro servidor de desarrollo 
-define('SERVER_NAME_DEV','sinca.local'); 
+define('SERVER_NAME_PROD','s1248-101');
+define('DIRECTORY_NAME_PRE_PROD','dev_sinca');
 //Obtenemos el nombre del servidor actual 
-$server=$_SERVER['SERVER_NAME']; 
-// change the following paths if necessary
-
-
-
+$server=gethostname();
+if($server==SERVER_NAME_PROD)
+{
+	$server=dirname(__FILE__);
+	$nuevo=explode(DIRECTORY_SEPARATOR,$server);
+	$num=count($nuevo);
+	if($nuevo[$num-3]==DIRECTORY_NAME_PRE_PROD)
+	{
+		defined('YII_DEBUG') or define('YII_DEBUG',true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+	}
+	else
+	{
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+	}
+}
+else
+{
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+}
 
 $yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/console.php';
-
-
-switch ($server)
-{
-	case SERVER_NAME_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',false);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
-		break;
-	case SERVER_NAME_PRE_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-		break;
-	case SERVER_NAME_DEV:
-	default:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-		break;
-}
-
-
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createConsoleApplication($config)->run();
