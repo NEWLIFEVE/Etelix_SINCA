a46862bd4020734f4a7bcd81b44c32e10965755f
edmaku <mark182182@gmail.com>
2014-05-21 17:38:43 -0430

    commit por merge

--- src/web/protected/views/balance/cicloIngresos.php
+++ src/web/protected/views/balance/cicloIngresos.php
@@ -332,7 +332,7 @@ $this->widget('zii.widgets.grid.CGridView',array(
                 ),
             ),
         array('name'=>'Acumulado',
-            'value'=>'Balance::Acumulado($data->Fecha,$data->CABINA_Id,false)',
+            'value'=>'Balance::Acumulado($data->FechaMes,$data->CABINA_Id,false)',
             'type'=>'text',
             'htmlOptions'=>array(
                 'style'=>'text-align: center; color: green;',
@@ -366,7 +366,7 @@ $this->widget('zii.widgets.grid.CGridView',array(
                 ),
             ),
         array('name'=>'SobranteAcum',
-            'value'=>'Balance::SobranteAcumulado($data->Fecha,$data->CABINA_Id,false)',
+            'value'=>'Balance::SobranteAcumulado($data->FechaMes,$data->CABINA_Id,false)',
             'type'=>'text',
             'htmlOptions'=>array(
                 'style'=>'text-align: center; color: green;',
